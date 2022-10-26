d8e0e0e322916f3b6a69eb6c6c82d6d0705bc60e
Luis Murilo <luiszkm@gmail.com>
2022-10-26 20:47:42 -0300

    created next app

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <h1 >
+      <Head>
+        <title>IgNews</title>
+      </Head>
+      hellosss
+      </h1>
+  )
+}
+
+export default Home
